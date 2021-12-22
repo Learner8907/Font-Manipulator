14bb3f89b99ef706ba92c6be63b217d52b398f9e
Learner8907 <72011994+Learner8907@users.noreply.github.com>
2021-12-22 15:09:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function modelLoaded(){
     console.log("Posenet is Initialized")
 }
 function gotPoses(results){
-    if(results.lenght > 0){
+    if(results.length > 0){
 
         console.log(results);
 
