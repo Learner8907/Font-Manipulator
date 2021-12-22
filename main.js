76d2819f593b23bbbc3cec2a9183127b1650bb6e
Learner8907 <72011994+Learner8907@users.noreply.github.com>
2021-12-22 10:40:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,6 +18,19 @@ function modelLoaded(){
 }
 function gotPoses(results){
     if(results.lenght > 0){
+
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
+}
+function draw(){
+    background("#F3F7F0");
+    textSize(difference);
+    fill("#A93F55");
+    text("Sharanya", 50, 400);
 }
